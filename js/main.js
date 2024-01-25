e93542b8507a1d224f507e63eb9002a7d8cf1e9d
Malena Montecino <mmontecinom2223@politecnics.barcelona>
2024-01-25 19:37:58 +0100

    update categorias

--- js/main.js
+++ js/main.js
@@ -32,39 +32,39 @@ function crearArrayPalabras() {
     //     "Cultura": ["traje", "idioma", "costumbre", "baile", "comida", "festividad", "mito", "religion", "creencias", "leyendas"]
     // }
      arrayPalabras = [
-        { palabra: "inglaterra", categoria: "Países", descripcion: "Inglaterra es un país en Europa conocido por su historia fascinante...", img: "inglaterra.jpg" },
-        { palabra: "francia", categoria: "Países", descripcion: "Francia es famosa por su Torre Eiffel y deliciosa comida...", img: "francia.jpg" },
-        { palabra: "italia", categoria: "Países", descripcion: "Italia es la cuna de la pizza y la pasta, con ciudades llenas de arte y historia...", img: "italia.jpg" },
-        { palabra: "china", categoria: "Países", descripcion: "China es conocida por su Gran Muralla y la celebración del Año Nuevo chino...", img: "china.jpg" },
-        { palabra: "mexico", categoria: "Países", descripcion: "México tiene vibrantes festivales como el Día de los Muertos, comida sabrosa como tacos...", img: "mexico.jpg" },
-        { palabra: "japon", categoria: "Países", descripcion: "Japón es un país en Asia con una rica herencia cultural, incluyendo la ceremonia del té y festivales como Hanami...", img: "japon.jpg" },
-        { palabra: "australia", categoria: "Países", descripcion: "Australia es conocida por su vida silvestre única, como canguros y koalas. Tiene hermosos paisajes...", img: "australia.jpg" },
-        { palabra: "brasil", categoria: "Países", descripcion: "Brasil es famoso por su carnaval vibrante, playas tropicales y selvas exuberantes...", img: "brasil.jpg" },
-        { palabra: "canada", categoria: "Países", descripcion: "Canadá es un país con impresionantes paisajes, como las Montañas Rocosas y las Cataratas del Niágara...", img: "canada.jpg" },
-        { palabra: "egipto", categoria: "Países", descripcion: "Egipto tiene una rica historia con pirámides antiguas y el río Nilo...", img: "egipto.jpg" },
-        { palabra: "rusia", categoria: "Países", descripcion: "Rusia es conocida por su vasto territorio y la Plaza Roja en Moscú...", img: "rusia.jpg" },
-        { palabra: "india", categoria: "Países", descripcion: "India es famosa por sus colores vibrantes, el Taj Mahal y su diversidad cultural...", img: "india.jpg" },
-        { palabra: "argentina", categoria: "Países", descripcion: "Argentina es conocida por su tango, carne asada y paisajes impresionantes como la Patagonia...", img: "argentina.jpg" },
+        { palabra: "inglaterra", categoria: "Países", descripcion: "Inglaterra es un país en Europa conocido por su historia fascinante...", img: "/images/imgDescripciones/inglaterra.jpg" },
+        { palabra: "francia", categoria: "Países", descripcion: "Francia es famosa por su Torre Eiffel y deliciosa comida...", img: "/images/imgDescripciones/francia.jpg" },
+        { palabra: "italia", categoria: "Países", descripcion: "Italia es la cuna de la pizza y la pasta, con ciudades llenas de arte y historia...", img: "/images/imgDescripciones/italia.jpg" },
+        { palabra: "china", categoria: "Países", descripcion: "China es conocida por su Gran Muralla y la celebración del Año Nuevo chino...", img: "/images/imgDescripciones/china.jpg" },
+        { palabra: "mexico", categoria: "Países", descripcion: "México tiene vibrantes festivales como el Día de los Muertos, comida sabrosa como tacos...", img: "/images/imgDescripciones/mexico.jpg" },
+        { palabra: "japon", categoria: "Países", descripcion: "Japón es un país en Asia con una rica herencia cultural, incluyendo la ceremonia del té y festivales como Hanami...", img: "/images/imgDescripciones/japon.jpg" },
+        { palabra: "australia", categoria: "Países", descripcion: "Australia es conocida por su vida silvestre única, como canguros y koalas. Tiene hermosos paisajes...", img: "/images/imgDescripciones/australia.jpg" },
+        { palabra: "brasil", categoria: "Países", descripcion: "Brasil es famoso por su carnaval vibrante, playas tropicales y selvas exuberantes...", img: "/images/imgDescripciones/brasil.jpg" },
+        { palabra: "canada", categoria: "Países", descripcion: "Canadá es un país con impresionantes paisajes, como las Montañas Rocosas y las Cataratas del Niágara...", img: "/images/imgDescripciones/canada.jpg" },
+        { palabra: "egipto", categoria: "Países", descripcion: "Egipto tiene una rica historia con pirámides antiguas y el río Nilo...", img: "/images/imgDescripciones/egipto.jpg" },
+        { palabra: "rusia", categoria: "Países", descripcion: "Rusia es conocida por su vasto territorio y la Plaza Roja en Moscú...", img: "/images/imgDescripciones/rusia.jpg" },
+        { palabra: "india", categoria: "Países", descripcion: "India es famosa por sus colores vibrantes, el Taj Mahal y su diversidad cultural...", img: "/images/imgDescripciones/india.jpg" },
+        { palabra: "argentina", categoria: "Países", descripcion: "Argentina es conocida por su tango, carne asada y paisajes impresionantes como la Patagonia...", img: "/images/imgDescripciones/argentina.jpg" },
         // Transporte
-        { palabra: "avion", categoria: "Transporte", descripcion: "Los aviones vuelan por el cielo y nos llevan a lugares lejanos. Son rápidos y emocionantes para viajar...", img: "avion.jpg" },
-        { palabra: "barco", categoria: "Transporte", descripcion: "Los barcos navegan en el agua y pueden llevarnos en emocionantes travesías por océanos y mares...", img: "barco.jpg" },
-        { palabra: "tren", categoria: "Transporte", descripcion: "Los trenes viajan sobre vías férreas y son geniales para explorar paisajes mientras se mueven sobre rieles...", img: "tren.jpg" },
-        { palabra: "bicicleta", categoria: "Transporte", descripcion: "Las bicicletas son divertidas y saludables, ideales para pasear al aire libre y explorar vecindarios...", img: "bicicleta.jpg" },
-        { palabra: "coche", categoria: "Transporte", descripcion: "Los coches nos llevan a todas partes por carreteras. Son rápidos y convenientes para viajar en familia...", img: "coche.jpg" },
-        { palabra: "camion", categoria: "Transporte", descripcion: "Los camiones son vehículos grandes que transportan mercancías por carretera. Son esenciales para el movimiento de bienes en todo el mundo...", img: "camion.jpg" },
-        { palabra: "helicoptero", categoria: "Transporte", descripcion: "Los helicópteros vuelan verticalmente y son útiles para misiones de rescate y transporte en lugares de difícil acceso...", img: "helicoptero.jpg" },
-        { palabra: "autobus", categoria: "Transporte", descripcion: "Los autobuses son medios de transporte colectivo que llevan a las personas a diferentes lugares en rutas planificadas...", img: "autobus.jpg" },
+        { palabra: "avion", categoria: "Transporte", descripcion: "Los aviones vuelan por el cielo y nos llevan a lugares lejanos. Son rápidos y emocionantes para viajar...", img: "/images/imgDescripciones/avion.jpg" },
+        { palabra: "barco", categoria: "Transporte", descripcion: "Los barcos navegan en el agua y pueden llevarnos en emocionantes travesías por océanos y mares...", img: "/images/imgDescripciones/barco.jpg" },
+        { palabra: "tren", categoria: "Transporte", descripcion: "Los trenes viajan sobre vías férreas y son geniales para explorar paisajes mientras se mueven sobre rieles...", img: "/images/imgDescripciones/tren.jpg" },
+        { palabra: "bicicleta", categoria: "Transporte", descripcion: "Las bicicletas son divertidas y saludables, ideales para pasear al aire libre y explorar vecindarios...", img: "/images/imgDescripciones/bicicleta.jpg" },
+        { palabra: "coche", categoria: "Transporte", descripcion: "Los coches nos llevan a todas partes por carreteras. Son rápidos y convenientes para viajar en familia...", img: "/images/imgDescripciones/coche.jpg" },
+        { palabra: "camion", categoria: "Transporte", descripcion: "Los camiones son vehículos grandes que transportan mercancías por carretera. Son esenciales para el movimiento de bienes en todo el mundo...", img: "/images/imgDescripciones/camion.jpg" },
+        { palabra: "helicoptero", categoria: "Transporte", descripcion: "Los helicópteros vuelan verticalmente y son útiles para misiones de rescate y transporte en lugares de difícil acceso...", img: "/images/imgDescripciones/helicoptero.jpg" },
+        { palabra: "autobus", categoria: "Transporte", descripcion: "Los autobuses son medios de transporte colectivo que llevan a las personas a diferentes lugares en rutas planificadas...", img: "/images/imgDescripciones/autobus.jpg" },
         // Cultura
-        { palabra: "traje", categoria: "Cultura", descripcion: "Los trajes representan la identidad cultural y la historia de un lugar. Cada país tiene trajes tradicionales únicos y coloridos...", img: "traje.jpg" },
-        { palabra: "idioma", categoria: "Cultura", descripcion: "Cada país tiene su propio idioma. Aprender diferentes idiomas nos ayuda a entender y apreciar diversas culturas...", img: "idioma.jpg" },
-        { palabra: "costumbre", categoria: "Cultura", descripcion: "Las costumbres son tradiciones especiales que la gente sigue, como saludar o celebrar festivales de manera única...", img: "costumbre.jpg" },
-        { palabra: "baile", categoria: "Cultura", descripcion: "Los bailes reflejan la alegría y la creatividad de una cultura. Cada país tiene danzas tradicionales emocionantes...", img: "baile.jpg" },
-        { palabra: "comida", categoria: "Cultura", descripcion: "La comida es una parte importante de la cultura. Cada país tiene platos deliciosos que cuentan historias sobre su gente y sus tradiciones...", img: "comida.jpg" },
-        { palabra: "festividad", categoria: "Cultura", descripcion: "Las festividades son celebraciones alegres que reúnen a la gente para compartir alegría y tradiciones únicas...", img: "festividad.jpg" },
-        { palabra: "mito", categoria: "Cultura", descripcion: "Los mitos son historias tradicionales que explican el origen de cosas o enseñan lecciones importantes...", img: "mito.jpg" },
-        { palabra: "religion", categoria: "Cultura", descripcion: "La religión es una parte importante de la cultura, con creencias espirituales y prácticas que guían la vida de las personas...", img: "religion.jpg" },
-        { palabra: "creencias", categoria: "Cultura", descripcion: "Las creencias son ideas fundamentales que la gente tiene sobre el mundo y la vida...", img: "creencias.jpg" },
-        { palabra: "leyendas", categoria: "Cultura", descripcion: "Las leyendas son historias fascinantes que han sido transmitidas a lo largo del tiempo y a menudo contienen elementos mágicos o heroicos...", img: "leyendas.jpg" }
+        { palabra: "traje", categoria: "Cultura", descripcion: "Los trajes representan la identidad cultural y la historia de un lugar. Cada país tiene trajes tradicionales únicos y coloridos...", img: "/images/imgDescripciones/traje.jpg" },
+        { palabra: "idioma", categoria: "Cultura", descripcion: "Cada país tiene su propio idioma. Aprender diferentes idiomas nos ayuda a entender y apreciar diversas culturas...", img: "/images/imgDescripciones/idioma.jpg" },
+        { palabra: "costumbre", categoria: "Cultura", descripcion: "Las costumbres son tradiciones especiales que la gente sigue, como saludar o celebrar festivales de manera única...", img: "/images/imgDescripciones/costumbre.jpg" },
+        { palabra: "baile", categoria: "Cultura", descripcion: "Los bailes reflejan la alegría y la creatividad de una cultura. Cada país tiene danzas tradicionales emocionantes...", img: "/images/imgDescripciones/baile.jpg" },
+        { palabra: "comida", categoria: "Cultura", descripcion: "La comida es una parte importante de la cultura. Cada país tiene platos deliciosos que cuentan historias sobre su gente y sus tradiciones...", img: "/images/imgDescripciones/comida.jpg" },
+        { palabra: "festividad", categoria: "Cultura", descripcion: "Las festividades son celebraciones alegres que reúnen a la gente para compartir alegría y tradiciones únicas...", img: "/images/imgDescripciones/festividad.jpg" },
+        { palabra: "mito", categoria: "Cultura", descripcion: "Los mitos son historias tradicionales que explican el origen de cosas o enseñan lecciones importantes...", img: "/images/imgDescripciones/mito.jpg" },
+        { palabra: "religion", categoria: "Cultura", descripcion: "La religión es una parte importante de la cultura, con creencias espirituales y prácticas que guían la vida de las personas...", img: "/images/imgDescripciones/religion.jpg" },
+        { palabra: "creencias", categoria: "Cultura", descripcion: "Las creencias son ideas fundamentales que la gente tiene sobre el mundo y la vida...", img: "/images/imgDescripciones/creencias.jpg" },
+        { palabra: "leyendas", categoria: "Cultura", descripcion: "Las leyendas son historias fascinantes que han sido transmitidas a lo largo del tiempo y a menudo contienen elementos mágicos o heroicos...", img: "/images/imgDescripciones/leyendas.jpg" }
       ];
 }
 
@@ -107,13 +107,15 @@ function timer() {
 // }
 function seleccionarPalabraRandom(arrayPalabras) {
     // Obtener un índice aleatorio de la array
-    numeroRandom = Math.floor(Math.random() * arrayPalabras.length);
-  
+   // numeroRandom = Math.floor(Math.random() * arrayPalabras.length);
+  numeroRandom = 0;
     // Obtener la palabra seleccionada
     palabraSeleccionada = arrayPalabras[numeroRandom].palabra;
     palabraDescripcion = arrayPalabras[numeroRandom].descripcion;
     palabraCategoria = arrayPalabras[numeroRandom].categoria;
-    palabraImagen = arrayPalabras[numeroRandom].imagen;
+    palabraImagen = arrayPalabras[numeroRandom].img;
+
+    
     
     // // Mostrar la categoría y la descripción asociada
     // document.getElementById('palabra').innerHTML = "<strong>Palabra: </strong>" + palabraSeleccionada;
@@ -327,6 +329,20 @@ function informacion(){
    // Mostrar la categoría y la descripción asociada
    document.getElementById('palabra').innerHTML = "<strong>Palabra: </strong>" + palabraSeleccionada;
    document.getElementById('categoria1').innerHTML = "<strong>Categoría: </strong>" + palabraCategoria;
-   document.getElementById('descripcion').innerHTML = "<strong>Descripción: </strong>" + palabraDescripcion;
-   document.getElementById('imagen').innerHTML = "<strong>Imagen: </strong>" + palabraImagen;
+   document.getElementById('descripcion').innerHTML = "<strong>Descripción: </strong> <br>" + palabraDescripcion;
+   //document.getElementById('imagen').innerHTML = "<strong>Imagen: </strong>" + palabraImagen;
+   
+    // Obtener el elemento de la imagen
+    let imagenElement = document.getElementById('imagen');
+
+    // Verificar si la imagen existe en la ruta proporcionada
+    if (palabraImagen) {
+        // Establecer la ruta de la imagen
+        imagenElement.src = palabraImagen;
+        // Mostrar la imagen
+        imagenElement.style.display = 'block';
+    } else {
+        // Ocultar la imagen si no hay ruta proporcionada
+        imagenElement.style.display = 'none';
+    }
 }
